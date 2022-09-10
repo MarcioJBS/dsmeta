373356af2ac420c39c03fb6e0bb6d2ff963819a0
MarcioJBS <marcio_barreto_uch@hotmail.com>
2022-09-10 14:15:06 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -25,6 +25,6 @@ public class SaleService {
         LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
         LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
     	
-        return repository.findSales(min, max,pageable);
+        return repository.findSales(min, max, pageable);
 	}
 }
