dfd894a56ab3ecd4da535025b8edb8937ed9cfe6
MarcioJBS <marcio_barreto_uch@hotmail.com>
2022-09-11 12:33:05 -0300

    Toast

--- frontend/src/componentes/NotificationsButton/index.tsx
+++ frontend/src/componentes/NotificationsButton/index.tsx
@@ -2,6 +2,7 @@ import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import axios from 'axios';
 import './styles.css'
+import { toast } from 'react-toastify';
 
 type Props = {
     saleId: number;
@@ -11,8 +12,9 @@ type Props = {
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log("SUCESSO");
-        })
+            toast.info("SMS enviado com sucesso");
+
+        });
 }
 
 function NotificationButton({ saleId }: Props) {
